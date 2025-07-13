d3085923e2a489a9aa2d808bfcad35ed0d00fe9b
Peeter Joot <peeter@lemurianlabs.com>
2025-07-13 09:59:09 -0400

    deleted:    GenerateTrainingData.sh SequenceGuesserData.sh -- experiment with a few output options.

--- SequenceGuesserData.sh
+++ SequenceGuesserData.sh
@@ -3,4 +3,10 @@
 t = Table[{RandomReal[{0.8, 1.05}], RandomReal[{0.8, 1.05}],
   RandomReal[{0, 2}], RandomReal[{0, 2}]}, {i, 0, 10}];
 
-Export["trainingdata.json", t]
+Export["trainingdata.txt", t]  (* ugly: missing newline *)
+
+(* could do:
+Export["trainingdata.txt", StringJoin[ExportString[t, "CSV"], "\n"]]
+
+-- but that's doesn't have the {} delimited lines of the original above.
+*)
